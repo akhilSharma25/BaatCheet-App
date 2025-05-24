89b3c568eaf0fd069c49b1384ce89a1298d45f12
Akhil Sharma <139208480+akhilSharma25@users.noreply.github.com>
2025-05-24 22:23:32 +0530

    Update Onboarding.jsx

--- frontend/src/pages/Onboarding.jsx
+++ frontend/src/pages/Onboarding.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import useAuthUser from "../hooks/useAuthUser";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
-// import { completeOnboarding } from "../LIB/api.js";
+import { completeOnboarding } from "../LIB/api.js";
 import {
   CameraIcon,
   LoaderIcon,
