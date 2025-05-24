b77c657b764705fd40910b3b207b147d60caf506
AKhil Sharma <akhilkatyan25b@gmail.com>
2025-05-24 20:24:22 +0530

    Hl

--- frontend/src/LIB/axios.js
+++ frontend/src/LIB/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 
-const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:3000/api":"/api"
+const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:3000/api":"https://baatcheet-app-aemv.onrender.com/api"
 export const axiosInstance=axios.create({
     baseURL:BASE_URL,
     withCredentials:true
