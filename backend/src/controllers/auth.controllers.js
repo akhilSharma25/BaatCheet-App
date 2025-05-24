fc1e933a2e9634f79d850b68e910751098183472
Akhil Sharma <139208480+akhilSharma25@users.noreply.github.com>
2025-05-24 22:06:27 +0530

    Update auth.controllers.js

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -63,7 +63,7 @@ export async function signup(req, res) {
       maxAge: 10 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       sameSite: "strict",
-      secure: process.env.NODE_ENV === "development",
+      secure: true,
     });
 
     res.status(201).json({ success: true, user: newUser });
@@ -103,7 +103,7 @@ export async function login(req, res) {
       maxAge: 10 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       sameSite: "strict",
-      secure: process.env.NODE_ENV === "development",
+      secure: true,
     });
 
     res.status(200).json({ success: true, userExist });
